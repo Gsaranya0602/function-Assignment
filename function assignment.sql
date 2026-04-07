d764afc63c5e3fa46e33a7a6432b5e4db1bd8418
Gsaranya0602 <saranyag1103@gmail.com>
2026-04-07 12:12:01 +0530

    Add files via upload

---
+++ function assignment.sql
@@ -0,0 +1,20 @@
+use assignment1;
+#  Create a function to return student mark for a given student name.
+select* from students;
+
+DELIMITER $$
+create function getStudentMark( name  VARCHAR(50))
+RETURNS INT
+DETERMINISTIC
+BEGIN 
+DECLARE std_mark INT ;
+SELECT  marks INTO std_mark FROM students where student_name=name;
+RETURN std_mark;
+END$$
+DELIMITER ;
+
+SELECT getStudentMark('priya');
+
+
+
+
